929c11709150de4b31829cfb166182455f5bc97e
hugosenari <hugosenari@gmail.com>
2022-03-23 23:04:29 -0300

    feat(nimpkgs): remove lock

--- v1_0_1/flake.nix
+++ v1_0_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Neverwinter Nights 1 data accessor library'';
+  description = ''nwn1 development tools'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-neverwinter-v1_0_1.flake = false;
-  inputs.src-neverwinter-v1_0_1.ref   = "refs/tags/v1.0.1";
-  inputs.src-neverwinter-v1_0_1.owner = "niv";
-  inputs.src-neverwinter-v1_0_1.repo  = "neverwinter.nim";
-  inputs.src-neverwinter-v1_0_1.type  = "github";
+  inputs.src-neverwinter_utils-v1_0_1.flake = false;
+  inputs.src-neverwinter_utils-v1_0_1.ref   = "refs/tags/v1.0.1";
+  inputs.src-neverwinter_utils-v1_0_1.owner = "niv";
+  inputs.src-neverwinter_utils-v1_0_1.repo  = "neverwinter.nim";
+  inputs.src-neverwinter_utils-v1_0_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-neverwinter-v1_0_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-neverwinter_utils-v1_0_1"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-neverwinter-v1_0_1";
+    src  = deps."src-neverwinter_utils-v1_0_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
